fb9d8edfb6d17204cb73ff31b066cd49f7d8063a
crackfield <crackfield777@gmail.com>
2023-01-13 19:27:19 +0200

    Manage app to use localStorage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit'
-import notesReducer from '../features/notesSlice'
+import { Middleware, configureStore } from '@reduxjs/toolkit';
+import notesReducer from '../features/notesSlice';
+
+const addToLocalStorage: Middleware = (store) => (next) => (action) => {
+  next(action);
+  localStorage.setItem(
+    'notes',
+    JSON.stringify(store.getState().notes.value) || '[]'
+  );
+};
 
 export const store = configureStore({
   reducer: {
     notes: notesReducer,
   },
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(addToLocalStorage),
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
--- src/features/notesSlice.ts
+++ src/features/notesSlice.ts
@@ -1,37 +1,40 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import notes from '../data/notes.json'
-import { EditedNote, Note, NoteState } from '../app/types'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import notes from '../data/notes.json';
+import { EditedNote, Note, NoteState } from '../app/types';
 
 const initialState: NoteState = {
-  value: notes as Note[],
-}
+  value: localStorage.getItem('notes')
+    ? (JSON.parse(localStorage.getItem('notes') || '[]') as Note[])
+    : (notes as Note[]),
+};
 
 export const notesSlice = createSlice({
   name: 'notes',
   initialState,
   reducers: {
     addNote: (state, action: PayloadAction<Note>) => {
-      state.value.push(action.payload)
+      state.value.push(action.payload);
     },
     editNote: (state, action: PayloadAction<EditedNote>) => {
-      const myNote = state.value.find((note) => note.id === action.payload.id)!
-      const index = state.value.indexOf(myNote)
+      const myNote = state.value.find((note) => note.id === action.payload.id)!;
+      const index = state.value.indexOf(myNote);
       state.value[index] = {
         ...state.value[index],
         title: action.payload.title,
         category: action.payload.category,
         content: action.payload.content,
-      }
+      };
     },
     archiveNote: (state, action: PayloadAction<string>) => {
-      const myNote = state.value.find((note) => note.id === action.payload)
-      myNote!.archived = !myNote!.archived
+      const myNote = state.value.find((note) => note.id === action.payload);
+      myNote!.archived = !myNote!.archived;
     },
     deleteNote: (state, action: PayloadAction<string>) => {
-      state.value = state.value.filter((note) => note.id !== action.payload)
+      state.value = state.value.filter((note) => note.id !== action.payload);
     },
   },
-})
+});
 
-export const { addNote, editNote, archiveNote, deleteNote } = notesSlice.actions
-export default notesSlice.reducer
+export const { addNote, editNote, archiveNote, deleteNote } =
+  notesSlice.actions;
+export default notesSlice.reducer;
